3af32896c06930464534c3161813d9dddf545443
Zack <zachary.chase@gmail.com>
2014-12-14 14:20:49 -0800

    removed garbage

--- experiments/filesys/contention/blahblah.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {11..30}
-do
-	new=foo$i.txt
-	cp foo1.txt $new
-    echo "finished" $new
-done
--- experiments/filesys/contention/run1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {0..5}
-do
-	run="./cont 1"
-	$run
-done
