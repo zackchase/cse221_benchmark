167cc10b289217ab401fd0bf0d9bfc4d468b0dba
hannahchen <bananaradiation@gmail.com>
2014-12-14 15:52:14 -0800

    part 2 editing

--- experiments/memory/read_band.c
+++ experiments/memory/read_band.c
@@ -25,7 +25,8 @@ int main() {
     unsigned long long coldstart = 0;
     unsigned long long end = 0;
     unsigned long long start = 0;
-    int i, sum;
+    int i;
+    int sum = 0;
 
     // allocate some array. doesn't matter if empty
     int* arr = (int*)malloc(sizeof(int) * (1 << EXPONENT));
@@ -33,12 +34,15 @@ int main() {
     coldstart = rdtsc();
 
     start = rdtsc();
-    // walk through the entire array
+    // walk through the entire array, read value
     for (i = 0; i < (1<<EXPONENT); i++){
-        // dead code
-        arr[i] = 12345;
-        // non dead code if compiler opts?
-        // sum += arr[i];
+        sum += arr[i];
+    
+    // with loop unrolling
+    // for (i = 0; i < (1<<EXPONENT); i+=10){
+    //     sum += (arr[i] + arr[i+1] + arr[i+3] +
+                // arr[i+4] + arr[i+5] + arr[i+6] +
+                // arr[i+7] + arr[i+8] + arr[i+9]);
     }
     end = rdtsc();
     printf("%llu\n", (end - start);
---
+++ experiments/memory/write_band.c
@@ -0,0 +1,71 @@
+#include <time.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <inttypes.h>
+
+#define THRESHOLD 10000000000000000
+
+static __inline__ unsigned long long int rdtsc(void)
+{
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
+
+int* make_array(int size) {
+    int* arr = malloc(size * sizeof(int));
+    int i;
+    for (i = 0; i < size; i++) {
+        arr[i] = rand() % size;
+    }
+    return arr;
+}
+
+
+int main() {
+	
+	int* arr;
+	int i;
+	int sum = 0;
+	unsigned long long start = 0;
+	unsigned long long end = 0;
+
+	int size = 1024 * 1024 * 16 / 4;
+	arr = make_array(size);
+
+	FILE *fp = fopen("garbage.txt", "a");   //open file for writing data
+	unsigned long long coldstart = rdtsc();
+	start = rdtsc();
+
+	// with loop unrolling
+	for (i = 0; i < size; i += 10) {
+		arr[i] = 123;
+		arr[i+1] = 123;
+		arr[i+2] = 123;
+		arr[i+3] = 123;
+		arr[i+4] = 123;
+		arr[i+5] = 123;
+		arr[i+6] = 123;
+		arr[i+7] = 123;
+		arr[i+8] = 123;
+		arr[i+9] = 123;
+	}
+	// no loop unrolling
+	// for (i = 0; i < size; i++) {
+	// 	arr[i] = 123;
+	// }	
+	end = rdtsc();
+	
+	if ((end-start) < THRESHOLD) {
+		// fprintf(fp, "%d\n", sum);
+		printf("%llu\n", (end-start));
+	}
+    fclose(fp);
+    free(arr);
+	return 0;
+}
